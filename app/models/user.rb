e46e155538271484e7547ffc11bd327568411218
Phillip Chen <phillipchen@gmail.com>
2011-10-11 16:39:16 -0700

    user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@
 #
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  
+  email_regex = /\A[\w+.\-]+\@[\-0-9a-zA-Z.]+\.(org|edu|jp|com)\z/i
+  
+  validates :name,  :presence => true, 
+                    :length   => { :maximum => 50}
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 end
