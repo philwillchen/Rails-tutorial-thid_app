a712e0811f36d7d4049330dbe3edabb17c2181a5
Phillip Chen <phillipchen@gmail.com>
2011-10-17 14:49:58 -0700

    finished user signup lesson 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+
   def new
     @title = "Sign up"
     @user = User.new
   end
   
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
+  def create
+#    raise params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module ThirdApp
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :secret_code]
 
     # Enable the asset pipeline
     config.assets.enabled = true
