e489c7608e064ed448ec4a6a14ff22c9e37aca59
Phillip Chen <phillipchen@gmail.com>
2011-10-25 13:57:34 -0700

    editing and updating users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -174,4 +174,15 @@ describe UsersController do
       end
     end
   end
+  
+  describe "authentication of edit/update actions" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should deny access to 'edit'" do
+      get :edit, :id => @user
+      response.should redirect_to(signin_path)
+    end
+  end
 end
