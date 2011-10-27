0df42db83111768a58ebda25c76da3f27d54262f
Phillip Chen <phillipchen@gmail.com>
2011-10-27 14:54:13 -0700

    users index page with pagination lesson 10

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -156,6 +156,23 @@ describe User do
       end
     end
   end
+  
+  describe "admin attributes" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to admin"
+  end
 end
 
 
