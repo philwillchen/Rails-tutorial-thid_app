a3692d7163d6022458038a3f06a78d7998fa2748
Phillip Chen <phillipchen@gmail.com>
2011-10-28 13:50:47 -0700

    added a micropost model with user associations lesson 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,8 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    User.find(params[:id]).destroy
+    @user.destroy
+    #User.find(params[:id]).destroy
     redirect_to users_path, :flash => { :success => "User destroyed." }
   end
   
@@ -62,7 +63,7 @@ class UsersController < ApplicationController
     end
     
     def admin_user
-      user = User.find(params[:id])
-      redirect_to(root_path) if (!current_user.admin? || current_user?(user))
+      @user = User.find(params[:id])
+      redirect_to(root_path) if (!current_user.admin? || current_user?(@user))
     end
 end
